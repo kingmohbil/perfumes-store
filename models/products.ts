9870c3b1ff4291ef343df3a76c8bc9af7dc6b6b2
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-26 00:44:27 +0300

    Edited the database schema to include additionl information

--- models/products.ts
+++ models/products.ts
@@ -3,8 +3,34 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
-  name: 'string',
-  image: Schema.Types.String,
+  name: {
+    type: 'string',
+    minLength: 1,
+    maxLength: 20,
+    required: true,
+  },
+  image: 'string',
+  price: {
+    type: 'number',
+    min: 10,
+    required: true,
+  },
+  description: {
+    type: 'string',
+    minLength: '10',
+    maxLength: '160',
+    required: true,
+  },
+  category: {
+    type: 'string',
+    values: ['men', 'women', 'unisex'],
+    required: true,
+  },
+  rating: {
+    type: 'number',
+    min: 1,
+    max: 5,
+  },
 });
 
 export default mongoose.model('products', ProductSchema);
