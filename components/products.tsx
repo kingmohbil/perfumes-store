45272142b0545b0299ebd3c64fed92c0243a5cc7
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-26 00:48:32 +0300

    Added the products container

---
+++ components/products.tsx
@@ -0,0 +1,13 @@
+import StyledProducts from './styles/styled_products';
+import Card from './product_card';
+export default function Products() {
+  return (
+    <StyledProducts>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </StyledProducts>
+  );
+}
