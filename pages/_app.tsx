56d2784301520f344232789b5acc8546f9433653
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-27 02:18:38 +0300

    Added the provider so all the pages can access the state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { store } from 'lib/store/store';
+import { Provider } from 'react-redux';
 import '@/styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  );
 }
