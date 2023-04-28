109e0d8253986c354bd10a40ae27eeb53781ea26
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-28 18:59:49 +0300

    Added the state for the components

--- components/cart_item.tsx
+++ components/cart_item.tsx
@@ -1,15 +1,23 @@
 import StyledItem from './styles/styled_cart_item';
-type PropsType = {
-  name: string;
-};
-export default function CartItem({ name }: PropsType) {
+import { useDispatch } from 'react-redux';
+import { addItem } from 'lib/store/slices/cart_slice';
+import CartType from 'types/cart_interface';
+export default function CartItem(props: CartType) {
+  const dispatch = useDispatch();
   return (
-    <StyledItem>
+    <StyledItem id={props.id}>
       <button className="decrement-btn"> - </button>
-      <p className="title">{name}</p>
-      <button className="increment-btn"> + </button>
-      <p className="count">1</p>
-      <p className="price">$300.00</p>
+      <p className="title">{props.name}</p>
+      <button
+        className="increment-btn"
+        onClick={() => {
+          dispatch(addItem(props));
+        }}
+      >
+        +
+      </button>
+      <p className="count">{props.count || 1}</p>
+      <p className="price">${(props.price * (props.count || 1)).toFixed(2)}</p>
     </StyledItem>
   );
 }
--- components/cart_modal.tsx
+++ components/cart_modal.tsx
@@ -1,21 +1,31 @@
 import StyledModal from './styles/styled_cart_modal';
 import CartItem from './cart_item';
+import { useSelector } from 'react-redux';
+import { RootState } from 'lib/store/store';
 type PropsType = {
   active: boolean;
   closeModal: () => void;
 };
 export default function CartModal({ active, closeModal }: PropsType) {
+  const cart = useSelector((state: RootState) => state.cart);
   return (
     <StyledModal className={active ? 'active' : ''}>
       <button className="close-btn" onClick={closeModal}>
         {' '}
         X{' '}
       </button>
-      <CartItem name="name of products" />
-      <CartItem name="product" />
+      {cart.items.map((item, index) => (
+        <CartItem
+          id={item.id}
+          key={index}
+          name={item.name}
+          count={item.count}
+          price={item.price}
+        />
+      ))}
       <hr />
       <div className="total-container">
-        <p>Total = $500.00 </p>
+        <p>Total = ${cart.total.toFixed(2)} </p>
       </div>
       <button className="checkout-btn">Checkout</button>
     </StyledModal>
