9a23c33d3b7b1e90ff61eb59b65a862d1c118b64
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-28 18:57:45 +0300

    edited the cards components, props was added

--- components/product_card.tsx
+++ components/product_card.tsx
@@ -1,33 +1,46 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useDispatch } from 'react-redux';
+import { addItem } from 'lib/store/slices/cart_slice';
 import StyledCard from './styles/styled_card';
+import CardType from 'types/products_interface';
 
-export default function Card() {
+export default function Card(props: CardType) {
+  const dispatch = useDispatch();
   return (
     <>
       <StyledCard>
-        <Link href="#">
+        <Link href={props.link || '#'}>
           <Image
-            src="/images/CoolWater.jpeg"
+            src={props.image || '/images/CoolWater.jpeg'}
             height="200"
             width="200"
             alt="cool water perfume"
           />
         </Link>
         <div className="card-details">
-          <h2 className="card-title">Cool Water</h2>
-          <p className="card-description">
-            Cool Water is a perfume by DAVIDOFF it`s best used in winter
-            occasions.
-          </p>
+          <h2 className="card-title">{props.name}</h2>
+          <p className="card-description">{props.description}</p>
           <div className="card-actions">
-            <h3 className="price">$200</h3>
+            <h3 className="price">${props.price.toFixed(2)}</h3>
             <div className="card-buttons">
-              <button className="cart">Add to Cart</button>
+              <button className="cart" onClick={addItemToCart}>
+                Add to Cart
+              </button>
             </div>
           </div>
         </div>
       </StyledCard>
     </>
   );
+
+  function addItemToCart() {
+    dispatch(
+      addItem({
+        id: props.id,
+        name: props.name,
+        price: props.price,
+      })
+    );
+  }
 }
--- components/products.tsx
+++ components/products.tsx
@@ -3,11 +3,51 @@ import Card from './product_card';
 export default function Products() {
   return (
     <StyledProducts>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        id="1"
+        name="Cool Water"
+        description="A perfect perfume in winter occasions"
+        image="/images/CoolWater.jpeg"
+        price={200}
+        category="men"
+        rating={1}
+      />
+      <Card
+        id="1"
+        name="Cool Water"
+        description="A perfect perfume in winter occasions"
+        image="/images/CoolWater.jpeg"
+        price={200}
+        category="men"
+        rating={1}
+      />
+      <Card
+        id="1"
+        name="Cool Water"
+        description="A perfect perfume in winter occasions"
+        image="/images/CoolWater.jpeg"
+        price={200}
+        category="men"
+        rating={1}
+      />
+      <Card
+        id="1"
+        name="Cool Water"
+        description="A perfect perfume in winter occasions"
+        image="/images/CoolWater.jpeg"
+        price={200}
+        category="men"
+        rating={1}
+      />
+      <Card
+        id="1"
+        name="Cool Water"
+        description="A perfect perfume in winter occasions"
+        image="/images/CoolWater.jpeg"
+        price={200}
+        category="men"
+        rating={1}
+      />
     </StyledProducts>
   );
 }
