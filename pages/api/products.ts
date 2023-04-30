21f5669977a3e2be6905c3ba07012fb0ae1c42f6
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-30 23:47:02 +0300

    Added multer to the API

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,17 +1,40 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnection from '../../lib/dbConnect';
-import Products from '../../models/products';
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === 'POST') {
-    dbConnection();
-    // Products.create({
-    //   name: 'welcome',
-    //   image: 'test image',
-    // });
-    res.status(201).json({});
-  } else res.status(404).json(null);
+import { NextApiRequest, NextApiResponse } from 'next';
+interface newRequest extends NextApiRequest {
+  file: {};
 }
+import nc from 'next-connect';
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function (req: NextApiRequest, file: any, cb: any) {
+    cb(null, 'public/uploads');
+  },
+  filename: function (req: NextApiRequest, file: any, cb: any) {
+    cb(
+      null,
+      `${new Date().toISOString().replace(/:/g, '-') + file.originalname}`
+    );
+  },
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const upload = multer({
+  storage,
+});
+
+const uploadFile = upload.single('productImage');
+
+const handler = nc()
+  .use(uploadFile)
+  .post(async (req: newRequest, res: NextApiResponse) => {
+    console.log(req?.file);
+    console.log(req.body);
+    res.json({ name: 'error' });
+  });
+
+export default handler;
