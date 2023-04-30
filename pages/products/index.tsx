fe4eabf30a04fb9a00a0437fd40efa1caa93b972
Mohammad Alwazzan <kingwazzan2@gmail.com>
2023-04-30 23:46:44 +0300

    used props to load the functionality

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -5,8 +5,15 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'lib/store/store';
 import { activate } from 'lib/store/slices/main_nav_slice';
+import ProductInterface from 'types/products_interface';
+import dbConnect from 'lib/dbConnect';
+const Products = require('models/products');
 
-export default function ProductsList() {
+type PropsType = {
+  products: ProductInterface[];
+};
+
+export default function ProductsList({ products }: PropsType) {
   const dispatch = useDispatch();
   const links = useSelector((state: RootState) => state.main_nav);
   const sideLinks = useSelector((state: RootState) => state.side_nav);
@@ -20,4 +27,19 @@ export default function ProductsList() {
       <ProductsContainer />
     </>
   );
+}
+
+export async function getStaticProps() {
+  await dbConnect();
+  const products = await Products.find({});
+  const jsonProducts = JSON.stringify(products);
+  const result = JSON.parse(jsonProducts);
+  console.log(`getStaticPropsCalled`);
+
+  return {
+    props: {
+      products: result,
+    },
+    revalidate: 20,
+  };
 }
